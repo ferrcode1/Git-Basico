0fecf478d1181841977462e87e6597876620d937
ferrcode <quicenouribe@gmail.com>
2024-09-25 13:22:25 -0500

    He creado una lista de comandos de git que he podido aprender hasta ahora en el curso, (Son 14 comandos 😎)

--- hellogit.py
+++ hellogit.py
@@ -1,1 +1,70 @@
 print("New Hello git!")  # New Hello git!")
+# 01:27 Hello git!
+
+""" 
+    COMANDO PARA CONFIGURAR EL ENTORNO DE TRABAJO
+    git config --global user.name "Your Name"
+    git config --global user.email "Your Email"
+    
+"""
+
+""" 
+    COMANDO PARA CREAR UN REPOSITORIO
+    git init
+"""
+
+""" 
+    COMANDO PARA VER EL ESTADO DEL REPOSITORIO
+    git status
+"""
+
+""" 
+    COMANDO PARA AGREGAR UN ARCHIVO AL AREA DE STAGING
+    git add .
+
+"""
+
+""" 
+    COMANDO PARA CREAR UN COMMIT CON UN MENSAJE
+    git commit -m "commit message"
+"""
+
+""" 
+    COMANDO PARA VER LOS COMMIT
+    git log
+"""
+
+""" 
+    COMANDO PARA VER LOS COMMIT FORMATEADOS
+    git log --graph
+    alias: git format
+"""
+
+""" 
+    COMANDO PARA VER LOS COMMIT FORMATEADOS
+    git log --pretty=oneline
+"""
+
+""" 
+    COMANDO PARA REMOVER UN ARCHIVO DEL AREA DE STAGING
+    git reset
+"""
+
+""" 
+    COMANDO PARA REMOVERSE DE RAMA DE TRABAJO
+    git checkout "hash del commit o rama al que queremos ir" 
+"""
+
+""" 
+    COMANDO PARA ELIMINAR O DESHACER RAMAS DE TRABAJO
+    git resest --hard "hash del commit o rama al que queremos ir (Se perdera todo el historial de commits realizado despues de este commit)"
+    
+    Para recuperar el historial de commits:
+    git reset --hard "hash del commit o rama al que queremos ir"
+"""
+
+""" 
+    COMANDO PARA LISTAR TODO EL HISTORIAL DE COMMITS
+    git reflog
+"""
+
